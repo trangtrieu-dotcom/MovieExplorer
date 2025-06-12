06b04e2577cfc7fbd29dc9fb574ec8e99cbad7c7
Trang <trang.trieu@hotmail.com>
2025-06-12 18:11:29 -0400

    add api+navbar+moviecard+popularmovie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import MovieCard from "../components/MovieCard.jsx";
+import { useState, useEffect } from "react";
+import { getPopularMovies } from "../services/api.js";
 
-const Home = () => {
+function Home() {
+  // state for movies, error, and loading
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // fetch popular movies 
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies(); 
+        setMovies(popularMovies);
+      } catch (error) {
+        setError("Failed to load movies", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-base-100 text-base-content">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Home Page (testing)</h1>
-      <button className="btn btn-primary mt-6">Get Started</button>
+    // TODO: will move to components/PopularMovies maybe
+    <div className="min-h-screen bg-base-200 px-8 py-8">
+      {/* section title */}
+      <h2 className="text-xl font-semibold text-white mb-6">Popular</h2>
+      {loading ? (
+        // loading state
+        <div className="text-white">Loading...</div>
+      ) : error ? (
+        // error state
+        <div className="text-red-400">{error}</div>
+      ) : (
+        // movie cards
+        // FIXME: not responsive enough
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
+          {/* IMPORTANT: maps over `movies` array, renders a MovieCard for each `movie` */}
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Home;
