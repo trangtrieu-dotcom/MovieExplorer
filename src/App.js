126480cafd0489e2a45f09873295af8c9124d7c2
Louis-TM <lchartier002@gmail.com>
2025-06-13 09:56:12 -0400

    MovieDetails, MovieHero, and MovieOverview all nearly done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/NavBar";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
   );
