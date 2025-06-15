4ab5ded335d695df744e6a8838f015ea5f53c94b
Trang Trieu <trang.trieu@hotmail.com>
2025-06-15 00:06:26 -0400

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const MovieCard = ({ movie }) => {
+  // https://developer.themoviedb.org/docs/image-basics
+  // https://developer.themoviedb.org/docs/image-languages
   const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
   // template from DaisyUI
   return (
     <div className="card bg-base-300 w-40 shadow-sm border border-base-200 rounded-lg overflow-hidden">
+      {/* image section */}
       <figure className="overflow-hidden">
         <img
           src={posterUrl}
@@ -13,13 +16,17 @@ const MovieCard = ({ movie }) => {
           className="w-full h-48 object-cover transition-transform duration-300 hover:scale-110"
         />
       </figure>
+      {/* car body section */}
       <div className="card-body p-3">
+          {/* rating */}
           <div className="badge badge-secondary text-xs">
             Rating: {movie.vote_average?.toFixed(1)}/10
           </div>
+        {/* title */}
         <h2 className="card-title text-white text-sm line-clamp-2 min-h-[2.5rem]">
           {movie.title}
         </h2>
+        {/* release date */}
         <p className="text-gray-400 text-xs">
           {movie.release_date ? new Date(movie.release_date).toLocaleDateString() : "Unknown release date"}
         </p>
