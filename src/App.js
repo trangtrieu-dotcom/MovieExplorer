ead7874cc8d27f4c78fd78089e52476c3ae45751
Shao Hang Li <shao.hang.li@outlook.com>
2025-06-18 02:06:45 -0400

    corrected the path

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route
-                path="/favorites-watchlist-recommendations"
+                path="/favorites-watchlist"
                 element={<Favorite />}
               />
               <Route path="/movie/:id" element={<MovieDetails />} />
